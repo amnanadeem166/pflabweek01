e89200a00dc87a18a7303b8f15f1d9fd1eb3b61c
amnanadeem166 <amnanadeem166@gmail.com>
2022-12-20 11:53:28 +0500

    pflab

---
+++ header.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+cout << "**************************************"<< endl;
+cout << "*                                    *"<< endl;
+cout << "*      University Admission          *"<< endl;
+cout << "*      Manangement System            *"<< endl;
+cout << "*                                    *"<< endl;
+cout << "*                                    *"<< endl;
+cout << "**************************************"<< endl;
+}
